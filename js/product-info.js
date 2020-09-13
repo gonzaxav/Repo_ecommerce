3144726ca0074e66e454fbc367a7c3adc8738410
gonzaxav <68866316+gonzaxav@users.noreply.github.com>
2020-09-13 19:30:44 -0300

    entrega 3 + css + extra cosas

--- js/product-info.js
+++ js/product-info.js
@@ -8,7 +8,6 @@ function showProduct(array, arrayComments) {
     let info = "";
     let imgs = "";
     let comments = "";
-    let score = "";
 
     info += `<h2>${product.name}</h2>
             <hr>
@@ -30,6 +29,46 @@ function showProduct(array, arrayComments) {
                 </div>
             </div>`;
 
+    let productScore = 0;
+
+    for (let comment in arrayComments) {
+        productScore += parseFloat(arrayComments[comment].score);
+    }
+    productScore = ((productScore / arrayComments.length * 10) / 10).toFixed(1);
+    let maxEstrellas = 5;
+    let puntos = "";
+
+    let productScoreRoundedTo05 = Math.round(productScore * 2) / 2;
+
+    for (let i = 1; i <= maxEstrellas; i++) {
+        if (i <= productScoreRoundedTo05) {
+            puntos += '<span class="fa fa-star checked"></span>';
+        }
+        else if (productScoreRoundedTo05 - i == -0.5) {
+            puntos += '<span class="fa fa-star-half-alt"></span>';
+        }
+        else {
+            puntos += '<span class="fa fa-star"></span>';
+        }
+    }
+
+    comments += `<div class="productAverageScore">
+                    <h3>Opiniones sobre el producto</h3>
+                    <div class="scoreStarAndPromedio">
+                        <span class="productScore">${productScore}</span>
+                        <span class="starAndPromedio">
+                            <span class="productScoreStars">${puntos}</span>
+                            <div>
+                                <span>Promedio entre</span>
+                                <span>${arrayComments.length}</span>
+                                <span>opiniones</span>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+                <br>
+                `;
+
     for (let comment in arrayComments) {
         let maxEstrellas = 5;
         let puntos = "";
@@ -44,12 +83,13 @@ function showProduct(array, arrayComments) {
         }
         comments += `<div style="text-align: left;">${puntos}</div>
                     <strong>${arrayComments[comment].user}</strong> dice:<br>
-                    <p>${arrayComments[comment].description}</p><br>`;
+                    <p>${arrayComments[comment].description}</p>
+                    <small>${arrayComments[comment].dateTime}</small><br><br>`;
     }
 
-    document.getElementById("contenido").innerHTML += info;
-    document.getElementById("imagenes").innerHTML += imgs;
-    document.getElementById("comentarios").innerHTML += comments;
+    document.getElementById("contenido").innerHTML = info;
+    document.getElementById("imagenes").innerHTML = imgs;
+    document.getElementById("comentarios").innerHTML = comments;
 
     document.getElementById("img01").addEventListener("mouseenter", function (e) {
         document.getElementById("img06").src = this.src;
@@ -81,4 +121,30 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showProduct(product, commentsArray);
         }
     })
+    document.getElementById("enviarNuevoComentario").addEventListener("click", function (e) {
+        let now = new Date();
+
+        let dateTime = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}
+                        ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+
+        let newComment = {
+            score: getRating(),
+            description: document.getElementById('nuevoComentarioTextoID').value,
+            user: JSON.parse(localStorage.getItem('User-Logged')).email,
+            dateTime: dateTime
+        };
+
+        commentsArray.push(newComment);
+
+        showProduct(product, commentsArray);
+    })
 });
+
+function getRating() {
+    var labels = document.getElementsByName('rating');
+    for (var i = 0; i < labels.length; i++) {
+        if (labels[i].checked) {
+            return parseInt(labels[i].value);
+        }
+    }
+}
